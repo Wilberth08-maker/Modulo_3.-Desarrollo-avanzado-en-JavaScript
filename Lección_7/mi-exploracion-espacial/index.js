903d930872be570fa7c8df1bc1c4496c8c0d1028
Wilberth Adair Martínez Puc <wilberthmtzpuc@gmail.com>
2025-06-18 19:40:07 -0500

    Se implemento el paquete cowsay en el código

--- Lección_7/mi-exploracion-espacial/index.js
+++ Lección_7/mi-exploracion-espacial/index.js
@@ -1,8 +1,12 @@
 const planetas = require('./planetas');
+const cowsay = require('cowsay');
 
 planetas.forEach((planeta) => {
-    console.log(`¡Planeta ${planeta.nombre} descubierto!`);
-    console.log(`Descripción: ${planeta.descripcion}`);
-    console.log(`Descubierto en: ${planeta.descubiertoEn}`);
+    console.log(cowsay.say({
+        text: `¡Planeta ${planeta.nombre} descubierto!\nDescripción: ${planeta.descripcion}\nAño: ${planeta.descubiertoEn}`,
+        e: "^^",
+        T: "U ",
+        f: "stegosaurus"
+    }));
     console.log("---");
 });
--- Lección_7/mi-exploracion-espacial/planetas.js
+++ Lección_7/mi-exploracion-espacial/planetas.js
@@ -2,12 +2,12 @@ const planetas = [
     {
         nombre: "Titán",
         descripcion: "La luna más grande de Saturno, con lagos de metano.",
-        descubiertoEn: "1655",
+        descubiertoEn: "1655"
     },
     {
         nombre: "Próxima Centauri b",
         descripcion: "Un exoplaneta rocoso en la zona habitable de su estrella.",
-        descubiertoEn: "2016",
+        descubiertoEn: "2016"
     },
     {
         nombre: "Kepler-452b",
