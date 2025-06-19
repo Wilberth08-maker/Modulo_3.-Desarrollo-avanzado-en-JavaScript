427fab2fdd3b2ad23effc376730c005364538b55
Wilberth Adair Martínez Puc <wilberthmtzpuc@gmail.com>
2025-06-18 19:25:17 -0500

    Se realizó la actividad de la lección 7 de campus y se installo cowsay

---
+++ Lección_7/mi-exploracion-espacial/planetas.js
@@ -0,0 +1,44 @@
+const planetas = [
+    {
+        nombre: "Titán",
+        descripcion: "La luna más grande de Saturno, con lagos de metano.",
+        descubiertoEn: "1655",
+    },
+    {
+        nombre: "Próxima Centauri b",
+        descripcion: "Un exoplaneta rocoso en la zona habitable de su estrella.",
+        descubiertoEn: "2016",
+    },
+    {
+        nombre: "Kepler-452b",
+        descripcion: "Un exoplaneta considerado un 'primo' de la Tierra, en la zona habitable de su estrella.",
+        descubiertoEn: "2015"
+    },
+    {
+        nombre: "Europa",
+        descripcion: "Luna de Júpiter cubierta por una capa de hielo, debajo podría haber un océano de agua líquido",
+        descubiertoEn: "1610"
+    },
+    {
+        nombre: "Proxima Centauri b",
+        descripcion: "Exoplaneta en la zona habitable de la estrella más cercana al Sol, Proxima Centauri.",
+        descubiertoEn: "2016"
+    },
+    {
+        nombre: "TRAPPIST-1e",
+        descripcion: "Uno de los planetas en la zona habitable del sistema TRAPPIST-1, con condiciones potencialmente aptas para la vida.",
+        descubiertoEn: "2017"
+    },
+    {
+        nombre: "LHS 1140 b",
+        descripcion: "Planeta rocoso en la zona habitable de una enana roja, con potencial para agua líquida en su superficie.",
+        descubiertoEn: "2017"
+    },
+    {
+        nombre: "Kepler-62f",
+        descripcion: "Exoplaneta en la zona habitable de su estrella, con tamaño similar a la Tierra.",
+        descubiertoEn: "2013"
+    }
+];
+
+module.exports = planetas;
